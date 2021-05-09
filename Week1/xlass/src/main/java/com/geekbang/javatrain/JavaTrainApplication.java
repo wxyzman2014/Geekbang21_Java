aa9be9ecca7241fae9028dc00d88847bf365804b
wuxingyu10 <wuxingyu@jd.com>
2021-05-09 16:02:55 +0800

    v

---
+++ Week1/xlass/src/main/java/com/geekbang/javatrain/JavaTrainApplication.java
@@ -0,0 +1,13 @@
+package com.geekbang.javatrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTrainApplication.class, args);
+	}
+
+}
