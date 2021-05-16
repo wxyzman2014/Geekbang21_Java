90c9d90e562f83501b34f3b951be15b3b32e672e
wuxingyu10 <wuxingyu@jd.com>
2021-05-17 01:11:25 +0800

    v

---
+++ Week2/httpclient/src/test/java/com/geekbang/javatrain/JavaTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbang.javatrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
